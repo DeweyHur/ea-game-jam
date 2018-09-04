57b251a78707b75658d3eb3aa86acb587bfd5624
Orange Dew <digitzetre@gmail.com>
2018-09-04 00:35:47 -0700

    Add Chips

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,79 @@
-import React, { Component } from 'react';
-import ProjectCard from './ProjectCard';
-
 import './Home.css';
 
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ProjectCard from './ProjectCard';
+import { Avatar, Chip, Autocomplete } from 'react-md';
+
+const WorksData = [
+  { title: 'Dragon Age: Castles', authors: ['Armando Antonnioni', 'Jethro Aquino', 'Ryan Ackert'], category: 'New IP' },
+  { title: 'Hippo Tutu', authors: ['Jayme Kielo', 'Steven Gosling'], category: 'New IP' },
+  { title: 'Pawstruck', authors: ['Tim Wang', 'Alvin Kwok', 'Alex Kolakowsk', 'Andrea Ma'], category: 'New IP' },
+  { title: 'Patch 11', authors: ['Daniel Forero', 'Cory Bailey', 'Gavin Card', 'Ross Baker', 'Holly Babaran'], category: 'New Feature' },
+  { title: 'REAplay', authors: ['Krista Parham', 'Will Wang', 'Dave Mcansh'], category: 'New Feature' },
+  { title: 'bEA', authors: ['Euphemia Wong', 'Sean Brouwer'], category: 'New Feature' },
+  { title: 'Star Wars: Gloom Walkers', authors: ['Jeremiah de Groot'], category: 'New Feature' },
+  { title: 'HUT Live Draft', authors: ['Josh Threlfall', 'Christopher Anderson', 'Aleksander Lebada', 'Craig Penner'], category: 'New Feature' },
+  { title: 'Star Wars: Underworld', authors: ['Alon Mizrahi', 'Brian Vidovic'], category: 'New Feature' },
+  { title: 'Birdseye', authors: ['Leo Teng', 'Patrick C Lawrence', 'Stanislav Folov', 'Sohrab Zabetian'], category: 'New Feature' },
+];
+
+const ChipsData = [
+  ..._.flatMap(WorksData, work => [
+    { category: 'T', label: `Title: ${work.title}`, filter: item => item.title === work.title },
+    ...work.authors.map(author => ({ category: 'A', label: `Author: ${author}`, filter: item => _.includes(item.authors, author) }))
+  ]),
+  ..._.keys(_.groupBy(WorksData, 'category')).map(type => ({ category: 'C', label: `Category: ${type}`, filter: item => item.type === type }))
+];
+
 export default class extends Component {
+  state = { selectedStates: [], filteredStates: ChipsData };
+
+  setNextState = (selectedStates) => {
+    this.setState({
+      ...this.state,
+      selectedStates,
+      filteredStates: _.without(ChipsData, selectedStates)
+    });
+  }
+
   render() {
-    const cards = Array.from(new Array(4), (_, index) => <ProjectCard />);
+    const { selectedStates, filteredStates } = this.state;
+
+    const filtersByCategory = _.groupBy(selectedStates, 'category');
+    const cards = WorksData
+      .filter(work => _.values(filtersByCategory).every(filters => filters.some(data => data.filter(work))))
+      .map(work => <ProjectCard work={work} />);
+    const chips = selectedStates.map(filter => <Chip removable
+      className="chip"
+      key={filter.label}
+      label={filter.label}
+      onClick={() => this.setNextState(_.without(selectedStates, filter))}
+      avatar={<Avatar>{filter.category}</Avatar>}
+    />);
+
     return (
-      <div className="Home-intro md-grid">
-        {cards}
+      <div className="Home-intro">
+        <div className="Home-filter">
+          {chips}
+          <Autocomplete
+            id="states-autocomplete"
+            label="Type Title, Author, or Category to search"
+            data={filteredStates}
+            dataLabel="label"
+            dataValue="label"
+            onAutocomplete={(_title, index, matches) => {
+              const state = matches[index];
+              const newStates = _.uniq([...selectedStates, state]);
+              this.setNextState(newStates);
+            }}
+            clearOnAutocomplete
+            deleteKeys="label"
+          />
+        </div>
+        <div className="md-grid">
+          {cards}
+        </div>
       </div>
     );
   }
--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -17,15 +17,15 @@ export default class extends Component {
 
   render() {
     const { popupVisible } = this.state;
-    const title = 'FIFA Tactics';
+    const { work: { title, authors, category } } = this.props;
 
     return (
       <Card className="ProjectCard md-cell md-cell--6 md-cell--8-tablet">
         <Media>
           <img src="http://www.fifplay.com/img/public/fifa-mobile-tactics.jpg" alt="FIFA Tactics" />
           <MediaOverlay>
-            <CardTitle title={title} subtitle="by Dewey Hur">
-              <Button className="md-cell--right" icon>favorite</Button>
+            <CardTitle title={title} subtitle={`by ${authors[0]} +${authors.length}`}>
+              <Button className="md-cell--right" flat>{category}</Button>
             </CardTitle>
           </MediaOverlay>
         </Media>
@@ -34,7 +34,11 @@ export default class extends Component {
           <Button className="md-cell-left" icon>comment</Button>
         </CardActions>
         <CardText expandable>
-          <h2>Description</h2>
+          <h2>{title}</h2>
+          <subtitle>by {authors.join(' ,')}</subtitle>
+          <h3>Category</h3>
+          <p>{category}</p>
+          <h3>Description</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eleifend odio.
       Vivamus quis quam eget augue facilisis laoreet. Aliquam egestas turpis pellentesque
@@ -55,7 +59,7 @@ export default class extends Component {
           onHide={this.hideConfirmPopup}
           actions={[
             <Button flat onClick={this.hideConfirmPopup}>Not Now</Button>,
-            <Button flat primary onClick={this.hideConfirmPopup}>Vote up !!</Button>      
+            <Button flat primary onClick={this.hideConfirmPopup}>Vote up !!</Button>
           ]}
         >
           <p>Do you want to vote <strong>{title}</strong> up?</p>
